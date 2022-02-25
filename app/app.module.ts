e3e15434a108fdb7c977b356f9b4b6493a84209d
kashifnizami@gmail.com <kashifnizami@gmail.com>
2022-02-25 11:46:38 +0100

    Until 21. LoginService

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,11 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './components/login/login.component';
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,9 +22,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
-   providers: [],
+   providers: [
+     LoginService
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
